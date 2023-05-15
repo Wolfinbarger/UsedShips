44259283ced6bd3f3cff34ab7edb6279a5888dc3
stefano zerega <stefano.japan93@gmail.com>
2023-05-15 18:02:25 +0900

    added validationg for booking, spaceship, spaceshipreview

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :spaceship_id
   belongs_to :user_id
+  validates :start_date, presence: true, date: true
+  validates :end_date, presence: true, date: true
+  validates :total_price, presence: true, numericality: { only_integer: true }
 end
--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,3 +1,8 @@
 class Spaceship < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :rate, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true
+  validates :crew, numericality: { only_integer: true }
+  validates :max_weight, numericality: { only_integer: true }
 end
