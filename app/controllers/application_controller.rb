fe11431b7fd1f4ec3a60f85179d11cbcd775f1c3
Daniel Scharf <id.scharfd@gmail.com>
2023-05-16 12:19:39 +0900

    added basic devise, cannot test yet need to update to test, will start a new branch for that

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
 end
